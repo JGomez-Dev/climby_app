4c6fbbd3d109d84516e918afd081f1641c38a30c
javiergf1991@gmail.com <javiergf1991@gmail.com>
2022-06-22 14:32:36 +0200

    Ahora el spinner de comunidades solo muestra 5 elementos.

---
+++ app/src/main/java/com/app/climby/utils/SpinnerExtension.java
@@ -0,0 +1,63 @@
+package com.app.climby.utils;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+import android.widget.LinearLayout;
+import android.widget.ListPopupWindow;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.app.climby.R;
+
+import java.lang.reflect.Field;
+
+public class SpinnerExtension extends Spinner {
+
+    public SpinnerExtension(@NonNull Context context) {
+        super(context);
+    }
+
+    public SpinnerExtension(@NonNull Context context, int mode) {
+        super(context, mode);
+    }
+
+    public SpinnerExtension(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SpinnerExtension(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public SpinnerExtension(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr, int mode) {
+        super(context, attrs, defStyleAttr, mode);
+    }
+
+    public SpinnerExtension(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr, int mode, int popupTheme) {
+        super(context, attrs, defStyleAttr, mode, popupTheme);
+    }
+
+    @Override
+    public void setAdapter(SpinnerAdapter adapter) {
+        super.setAdapter(adapter);
+        try {
+            Field popup = Spinner.class.getDeclaredField("mPopup");
+            popup.setAccessible(true);
+            // Get private mPopup member variable and try cast to ListPopupWindow
+            ListPopupWindow popupWindow = (ListPopupWindow) popup.get(this);
+            if (popupWindow != null) {
+                popupWindow.setHeight(this.getMeasuredHeight() * 5);
+            }
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+    }
+
+}
